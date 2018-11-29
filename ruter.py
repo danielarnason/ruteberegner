bde8ad1c233df3554e95a13b70b67bac4dda3ee9
danielarnason <danielarnason85@gmail.com>
2018-11-29 12:35:28 +0100

    kører en adressefil igennem og returnerer en geojson fil

--- ruter.py
+++ ruter.py
@@ -1,8 +1,11 @@
 import requests
 import pydawa
 import pandas as pd
+import json
+from tqdm import tqdm
+import re
 
-base_url = 'https://router.project-osrm.org/route/v1/foot/'
+base_url = 'http://10.65.130.183:5000/route/v1/foot/'
 
 test_skolekoordinat = '12.30752919,55.73275526'
 
@@ -18,16 +21,34 @@ def beregn_rute(fra, til):
 
 if __name__ == '__main__':
     data = read_data('export876867520626466480.csv')
-    data = data.sample(n=1, random_state=42)
-    
-    for idx, row in data.iterrows():
+    data = data.sample(n=300, random_state=42)
 
-        vejnavn =  row['adresse1'].split(' ')[0]
+    routes = {
+        'features': [],
+        'type': 'FeatureCollection',
+    }
+    
+    for idx, row in tqdm(data.iterrows()):
+
+        vejnavn_re = re.search(r'^(\D*)\s|,', row['adresse1'])
+        vejnavn = vejnavn_re.group(1)
+        # vejnavn =  row['adresse1'].split(' ')[0]
         husnr = row['husnr']
         postnr = row['postnr']
 
-        dawa_respons = pydawa.Adressesoeg(vejnavn, husnr, postnr, srid=4326).info()[0]
-        koordinater = str(dawa_respons['x']) + ',' + str(dawa_respons['y'])
+        dawa_respons = pydawa.Adressesoeg(vejnavn, husnr, postnr, srid=4326).info()
+        if len(dawa_respons) > 0:
+            koordinater = str(dawa_respons[0]['x']) + ',' + str(dawa_respons[0]['y'])
 
-        rute = beregn_rute(koordinater, test_skolekoordinat)
-        print(rute)
+            osrm_data = beregn_rute(koordinater, test_skolekoordinat)
+            rute_geom = osrm_data['routes'][0]['geometry']
+            feature = {
+                'geometry': rute_geom,
+                'id': idx,
+                'properties': {},
+                'type': 'Feature',
+            }
+            routes['features'].append(feature)
+
+    with open('test.geojson', 'w') as output:
+        json.dump(routes, output)
